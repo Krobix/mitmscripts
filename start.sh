475ecf04f978924475810c6998c36b04ebde6eac
Krobix <brendon.shaw8921@gmail.com>
2024-04-29 19:55:55 -0400

    Comment new stuff out, I don't want to deal with this rn

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ sudo iptables -t nat -A PREROUTING -s 10.0.0.0/8 -p tcp --dport 80 -j REDIRECT -
 echo "rule 2"
 sudo iptables -t nat -A PREROUTING -s 10.0.0.0/8 -p tcp --dport 443 -j REDIRECT --to-ports 8080
 echo "rule 3"
-sudo iptables -t mangle -A PREROUTING -m owner --uid-owner 999 -p tcp -j MARK --set-mark 500
+#sudo iptables -t mangle -A PREROUTING -m owner --uid-owner 999 -p tcp -j MARK --set-mark 500
 echo "rule 4"
-sudo iptables -t nat -A PREROUTING -m mark --mark 500 -p tcp -j REDIRECT --to-ports 8085
+#sudo iptables -t nat -A PREROUTING -m mark --mark 500 -p tcp -j REDIRECT --to-ports 8085
 sudo -u mitmproxy mitmproxy --mode transparent --showhost --set confdir=/xb/mitmproxy --set stream_large_bodies=5m -s /xb/mitmproxy/mitmscripts/trafficserver.py
