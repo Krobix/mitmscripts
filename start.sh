1aab91233ac4254f69a84398eadcd94e5bc10953
Krobix <brendon.shaw8921@gmail.com>
2024-04-29 19:36:16 -0400

    Fix (?)

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo iptables -t nat -A PREROUTING -s 10.0.0.0/8 -p tcp --dport 80 -j REDIRECT --to-ports 8080
 sudo iptables -t nat -A PREROUTING -s 10.0.0.0/8 -p tcp --dport 443 -j REDIRECT --to-ports 8080
-sudo iptables -t nat -A PREROUTING -m owner --uid-owner 999 -p tcp -j REDIRECT --to-ports 8085
-sudo -u mitmproxy mitmproxy --mode transparent --showhost --set confdir=/xb/mitmproxy --set stream_large_bodies=5m
+sudo iptables -A PREROUTING -m owner --uid-owner mitmproxy -p tcp -j MARK --set-mark 500
+sudo iptables -t nat -A PREROUTING -m mark --mark 500 -p tcp -j REDIRECT --to-ports 8085
+sudo -u mitmproxy mitmproxy --mode transparent --showhost --set confdir=/xb/mitmproxy --set stream_large_bodies=5m -s ~/mitmscripts/trafficserver.py
