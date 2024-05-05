f165c26538420977d8de9a47158e44c52bc91445
Krobix <brendon.shaw8921@gmail.com>
2024-05-05 14:33:15 -0400

    tls passthrough

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ echo "rule 3"
 #sudo iptables -t mangle -A PREROUTING -m owner --uid-owner 999 -p tcp -j MARK --set-mark 500
 echo "rule 4"
 #sudo iptables -t nat -A PREROUTING -m mark --mark 500 -p tcp -j REDIRECT --to-ports 8085
-sudo -u mitmproxy mitmproxy --mode transparent --showhost --set confdir=/xb/mitmproxy --set stream_large_bodies=5m #-s /xb/mitmproxy/mitmscripts/trafficserver.py
+sudo -u mitmproxy mitmproxy --mode transparent --showhost --set confdir=/xb/mitmproxy --set stream_large_bodies=5m -s tls_passthrough.py
