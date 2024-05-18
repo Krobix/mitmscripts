3f0ccf171c1eba76be1466a060caf8b2f692128b
Krobix <brendon.shaw8921@gmail.com>
2024-05-18 17:01:41 -0400

    add dns

--- start.sh
+++ start.sh
@@ -4,8 +4,6 @@ sudo iptables -t nat -A PREROUTING -s 10.0.0.0/8 -p tcp --dport 80 -j REDIRECT -
 echo "rule 2"
 sudo iptables -t nat -A PREROUTING -s 10.0.0.0/8 -p tcp --dport 443 -j REDIRECT --to-ports 8080
 echo "rule 3"
-#sudo iptables -t mangle -A PREROUTING -m owner --uid-owner 999 -p tcp -j MARK --set-mark 500
-echo "rule 4"
-#sudo iptables -t nat -A PREROUTING -m mark --mark 500 -p tcp -j REDIRECT --to-ports 8085
+sudo iptables -t nat -A PREROUTING -s 10.0.0.0/8 -p tcp --dport 53 -j REDIRECT --to-ports 8080
 cd /xb/mitmproxy/mitmscripts
-sudo -u mitmproxy mitmproxy --mode transparent --showhost --set confdir=/xb/mitmproxy --set stream_large_bodies=5m -s tls_passthrough.py
+sudo -u mitmproxy mitmproxy --mode transparent --showhost --set confdir=/xb/mitmproxy --set stream_large_bodies=5m -s tls_passthrough.py --dns-replace "xena.com A 10.66.66.1" --dns-replace "xena.com AAAA 10.66..66.1"
